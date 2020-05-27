82b30c62275a1cca1d5c79f07ec00f4475e95cd9
SOLOMON YISMAW <fantahunsolomon@gmail.com>
2020-05-27 03:52:59 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -35,6 +35,7 @@ def sorted_matrix
     [CONVENTIONAL_PRODUCE.sort, ORGANIC_PRODUCE.sort]
 end
 
+
 def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
